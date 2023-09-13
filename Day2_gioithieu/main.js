7a7c2503cc686250a5d55db23191f7197fb86f64
QuanPC131002 <quanthuoclao1310@gmail.com>
2023-09-13 23:13:40 +0700

    Add new project ECMAScript

---
+++ Day2_gioithieu/main.js
@@ -0,0 +1,15 @@
+// var myInfor = {
+//     name : "Quân",
+//     age : 22
+// };
+// var inforElenment = document.getElementById("myInfor");
+// inforElenment.innerHTML = `<h2>Tên: ${myInfor.name}</h2>
+// Tuổi : ${myInfor.age}
+// `;
+
+const Infor = {
+    name : "Tùng",
+    age : 23,
+};
+Infor.age = 24;
+console.log(Infor);
